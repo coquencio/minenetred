6d8b812febe57421c8335f370b74f5052452cb46
Jorge Domínguez de Anda <jorge.dm97@gmail.com>
2019-12-23 15:58:50 -0600

    post-time-entry-service

--- MinenetredApp/src/app/Services/TimeEntryService/time-entrty.service.ts
+++ MinenetredApp/src/app/Services/TimeEntryService/time-entrty.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { ITimeEntry } from '../../Interfaces/TimeEntryInterface';
 
 @Injectable({
   providedIn: 'root'
@@ -14,4 +15,12 @@ export class TimeEntrtyService {
       'https://localhost:44323/Entries/Hours/' + projectId + '?date=' + date,
       { withCredentials: true });
   }
+
+  AddTimeEntry(entry : ITimeEntry) : Observable<any>{
+    return this.httpClient.post(
+      'https://localhost:44323/Entries',
+      entry,
+      {withCredentials: true});
+  }
+
 }
